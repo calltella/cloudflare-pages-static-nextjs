e0f5bf6eb831c899e323ac7cb931a34c95496377
calltella <yuichi.asa@gmail.com>
2026-05-09 02:05:36 +0000

    pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 import Link from "next/link";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Hello World! This is the Home page</p>
-      <p>
-        Visit the <Link href="/about">About</Link> page.
-      </p>
-    </div>
-  );
-};
 
-export default Home;
+
+export default async function Page() {
+  return (<div>testpage</div>)
+}
+
